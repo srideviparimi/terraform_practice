70d335b91cb6139cd5107fd5eaf3706aeafe92f7
Sridevi <parimisridevi586@gmail.com>
2025-07-05 17:43:16 +1000

    terraform sample file

--- sample/sample.tf
+++ sample/sample.tf
@@ -1,5 +1,5 @@
 provider "aws" {}
-resource "aws_web_instance" "web_instance"{
+resource "aws_instance" "web_instance"{
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     tags={
