4e375ba768e9c96f6b94762e594a952e8bacf89f
Sridevi <parimisridevi586@gmail.com>
2025-07-05 21:19:38 +1000

    creating 3 web servers

--- sample/sample.tf
+++ sample/sample.tf
@@ -1,8 +1,39 @@
-provider "aws" {}
+provider "aws" {
+    region = "us-east-1"
+
+}
+variable "sName"{
+    default ={
+        frontend = "frontend",
+        mongodb = "mongodb",
+        catalogue="catalogue"
+
+    }
+}
+variable "ami_v" {
+    default= "ami-09c813fb71547fc4f"
+}
+variable "instance_type" {
+    default= "t3.micro"
+}
 resource "aws_instance" "web_instance"{
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
+    ami = var.ami_v
+    instance_type = var.instance_type
     tags={
-        Name = "HelloWorld"
+        Name = var.sName["frontend"]
+    }
+}
+resource "aws_instance" "web_instance"{
+    ami = var.ami_v
+    instance_type = var.instance_type
+    tags={
+      Name = var.sName["frontend"]
+    }
+}
+resource "aws_instance" "web_instance"{
+   ami = var.ami_v
+    instance_type =var.instance_type
+    tags={
+        Name = var.sName["frontend"]
     }
 }
