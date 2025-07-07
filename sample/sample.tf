00e68b8e24d1560f8a4ecb24c7a4ce2c88fa5e9f
Sridevi <parimisridevi586@gmail.com>
2025-07-07 18:59:17 +1000

    adjusted syntax for tfvars

--- sample/sample.tf
+++ sample/sample.tf
@@ -2,7 +2,8 @@ provider "aws" {
     region = "us-east-1"
 
 }
-variable {}
+variable "instance_type" {}
+variable "sNmae" {}
 resource "aws_instance" "web_instance1"{
     for_each = var.sName
     ami = "ami-09c813fb71547fc4f"
