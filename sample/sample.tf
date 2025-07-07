ea82d11ceb84f4be0bf7672597c02d713808dd8e
Sridevi <parimisridevi586@gmail.com>
2025-07-07 19:03:04 +1000

    adjusted syntax for tfvars

--- sample/sample.tf
+++ sample/sample.tf
@@ -7,7 +7,7 @@ variable "sName" {}
 resource "aws_instance" "web_instance1"{
     for_each = var.sName
     ami = "ami-09c813fb71547fc4f"
-    instance_type = each.value ["instance_type"]
+    instance_type = instance_type
     tags={
         Name = each.key
     }
