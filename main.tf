e7518960a2a249576cf6c9120da5fa2adf2c11ee
Sridevi <parimisridevi586@gmail.com>
2025-07-08 22:37:58 +1000

    added dns record resources inside the module

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "ec2" {
     for_each        = var.instances
     source          = "./modules/ec2/"
     instance_type   = each.value["instance_type"]
+    name            = each.key
     ami             = var.ami
     env             = var.env
     zone_name       = var.zone_name
