0c18b2bb88c3bfb06c7007ecfb2e782a06ead195
Sridevi <parimisridevi586@gmail.com>
2025-07-08 17:56:17 +1000

    added the variables inside the module

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-variable "instances" {}
-variable "ami" {}
-variable "env" {}
+
 module "ec2"{
     source          = "./modules/ec2/"
-    for_each        = var.instances 
 }
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,12 @@
-
+variable "instances" {}
+variable "ami" {}
+variable "env" {}
 
 resource "aws_instance" "web_instance"{
     for_each        = var.instances
     ami             = var.ami
-    instance_type   = each.key["instance_type"]
+    instance_type   = each.value["instance_type"]
     tags            = {
-        Name        =   ${each.key}-${var.env}
+        Name        =   "${each.key}-${var.env}"
     }
 }
