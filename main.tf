9fdf18e1256d5c773ed39b539f1a770473175729
Sridevi <parimisridevi586@gmail.com>
2025-07-08 17:22:50 +1000

    added the variables inside the module

--- main.tf
+++ main.tf
@@ -3,8 +3,5 @@ variable "ami" {}
 variable "env" {}
 module "ec2"{
     source          = "./modules/ec2/"
-    for_each        = var.instances
-    ami             = var.ami
-   
-    
+    for_each        = var.instances 
 }
