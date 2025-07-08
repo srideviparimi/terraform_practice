7ead34e09004fddae299a655f6c2642deb623f77
Sridevi <parimisridevi586@gmail.com>
2025-07-08 19:04:05 +1000

    added the variables inside the module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,8 @@
 
 
 resource "aws_instance" "web_instance"{
-    instance_type     = var.instance_type
+    instance_type    = var.instance_type
     ami              = var.ami
     
-    tags            = {
-        Name        =   "${each.key}-${var.env}"
-    }
+    
 }
