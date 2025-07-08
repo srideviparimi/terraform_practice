214a4bb127eae216111e32d11d52705198ab5cb3
Sridevi <parimisridevi586@gmail.com>
2025-07-08 17:05:55 +1000

    added the variables inside the module

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
-variable "instances" {}
-variable "ami" {}
-variable "instance_type" {}
+
 module "ec2"{
     source          = "./modules/ec2/"
     for_each        = var.instances
-    ami             = var.ami
-    instance_type   = var.instance_type
+    
     
 }
